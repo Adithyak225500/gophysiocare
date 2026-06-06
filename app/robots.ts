d01260fbaa5367ab39e38bcc784a97e22860ef07
Adithyak225500 <adithyakk225500@gmail.com>
2026-06-06 20:58:11 +0530

    Fix robots domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://gophysiocare.com/sitemap.xml",
+    sitemap: "https://gophysiocare.in/sitemap.xml",
   };
 }
