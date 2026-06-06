7767ff0b83d1061e2d3bf884abbfe7e0d9dac495
Adithyak225500 <adithyakk225500@gmail.com>
2026-06-06 20:52:38 +0530

    Fix sitemap domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://gophysiocare.com",
+      url: "https://gophysiocare.in",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
